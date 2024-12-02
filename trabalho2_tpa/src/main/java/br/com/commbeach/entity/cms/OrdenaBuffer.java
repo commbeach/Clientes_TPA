86130fbe7c084e789a4a3c245d675be9ee0ac31f
commbeach <113383800+commbeach@users.noreply.github.com>
2024-12-02 14:17:00 -0300

    Initial commit

---
+++ trabalho2_tpa/src/main/java/br/com/commbeach/entity/cms/OrdenaBuffer.java
@@ -0,0 +1,13 @@
+package br.com.commbeach.entity.cms;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class OrdenaBuffer {
+
+    private BufferDeClientes buffer;
+
+    public void linkabuffer(){}
+}
